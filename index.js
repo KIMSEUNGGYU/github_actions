e8a76124a23cbb598372954cb716a6cdc0535656
kimseunggyu <dev.seunggyu@gmail.com>
2020-06-22 13:53:43 +0900

    github actions test 1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", function (req, res) {
-  return res.send("Hello github actions");
+  return res.send("Hello github actions1 ");
 });
 
 app.listen(port, () => {
