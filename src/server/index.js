7a8ddc7b292435f6a0583994fa380b58b8b52e63
Ubuntu <ubuntu@ip-172-26-12-234.ap-northeast-2.compute.internal>
2020-08-03 11:15:21 +0000

    apply nginx

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 
 app.get("/", function (req, res) {
   return res.send("Hello github actions 10");
+  //return res.status(200).json(({message: "Hello github actions 10"}));
+});
+
+app.get("/api/v1/", function (req, res) {
+  return res.status(200).json(({message: "api/v1"}));
 });
 
 app.listen(port, () => {
